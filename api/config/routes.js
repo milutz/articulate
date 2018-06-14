3be9e97134d5826a0514c791703ffd9b047a6ff5
Mike Lutz <milutz@gmail.com>
2018-06-14 09:04:47 -0400

    code climate test 4
    code climate test 4

--- api/config/routes.js
+++ api/config/routes.js
@@ -7,3 +7,275 @@ module.exports = {
     entity: require('../modules/entity/config/entity.route'),
     context: require('../modules/context/config/context.route')
 };
+One of Code Climate’s primary focuses is to provide insight and visibility into how your codebase is changing over time. After we analyze a new commit, we’ll share our findings with you in various ways.
+
+Progress Events
+Your repository’s Progress page is the main hub for you to review Code Climate’s analysis data.
+
+When any of the following events occur during an analysis of your default branch, we’ll update your Progress report:
+
+An area of your codebase has its grade increase or decrease from one letter grade to another.
+
+New code is added to your codebase. The Progress report will show you its initial grade.
+
+Test coverage improves or declines.
+
+We’ll also post a weekly summary to your Progress report (on Monday), which shows information like how many files were changed, inserted, and deleted in the past seven days.
+
+The very first analysis that Code Climate runs on your repository (directly after the repository is added) will never result in any Progress report updates. Only the second and subsequent analyses can produce Progress report updates.
+Email and Integration Events
+We understand that you may not have time to check the Code Climate website every day, so we’ll also notify you via email and chatroom integrations. To reduce noise, we’re more conservative about when we send notifications.
+
+Below are all possible notifications that can result from an analysis of your default branch:
+
+An area of your codebase increases from a D or F to any other grade. For example, from a D to an A.
+
+An area of your codebase decreases to a D or F from any other grade. For example, from a B to an F.
+
+New code is added to your codebase and its initial grade is a D or F.
+
+Test coverage improves or declines.
+
+By default, emails for this event are disabled. You can enable them by editing your personal email preferences.
+By default, you’ll receive one weekly summary per repository that has received at least one commit in the past week. Weekly summaries are sent only via email, not via integration notifications.
+TABLE OF CONTENTS
+Progress Events
+Email and Integration Events
+One of Code Climate’s primary focuses is to provide insight and visibility into how your codebase is changing over time. After we analyze a new commit, we’ll share our findings with you in various ways.
+
+Progress Events
+Your repository’s Progress page is the main hub for you to review Code Climate’s analysis data.
+
+When any of the following events occur during an analysis of your default branch, we’ll update your Progress report:
+
+An area of your codebase has its grade increase or decrease from one letter grade to another.
+
+New code is added to your codebase. The Progress report will show you its initial grade.
+
+Test coverage improves or declines.
+
+We’ll also post a weekly summary to your Progress report (on Monday), which shows information like how many files were changed, inserted, and deleted in the past seven days.
+
+The very first analysis that Code Climate runs on your repository (directly after the repository is added) will never result in any Progress report updates. Only the second and subsequent analyses can produce Progress report updates.
+Email and Integration Events
+We understand that you may not have time to check the Code Climate website every day, so we’ll also notify you via email and chatroom integrations. To reduce noise, we’re more conservative about when we send notifications.
+
+Below are all possible notifications that can result from an analysis of your default branch:
+
+An area of your codebase increases from a D or F to any other grade. For example, from a D to an A.
+
+An area of your codebase decreases to a D or F from any other grade. For example, from a B to an F.
+
+New code is added to your codebase and its initial grade is a D or F.
+
+Test coverage improves or declines.
+
+By default, emails for this event are disabled. You can enable them by editing your personal email preferences.
+By default, you’ll receive one weekly summary per repository that has received at least one commit in the past week. Weekly summaries are sent only via email, not via integration notifications.
+TABLE OF CONTENTS
+Progress Events
+Email and Integration Events
+One of Code Climate’s primary focuses is to provide insight and visibility into how your codebase is changing over time. After we analyze a new commit, we’ll share our findings with you in various ways.
+
+Progress Events
+Your repository’s Progress page is the main hub for you to review Code Climate’s analysis data.
+
+When any of the following events occur during an analysis of your default branch, we’ll update your Progress report:
+
+An area of your codebase has its grade increase or decrease from one letter grade to another.
+
+New code is added to your codebase. The Progress report will show you its initial grade.
+
+Test coverage improves or declines.
+
+We’ll also post a weekly summary to your Progress report (on Monday), which shows information like how many files were changed, inserted, and deleted in the past seven days.
+
+The very first analysis that Code Climate runs on your repository (directly after the repository is added) will never result in any Progress report updates. Only the second and subsequent analyses can produce Progress report updates.
+Email and Integration Events
+We understand that you may not have time to check the Code Climate website every day, so we’ll also notify you via email and chatroom integrations. To reduce noise, we’re more conservative about when we send notifications.
+
+Below are all possible notifications that can result from an analysis of your default branch:
+
+An area of your codebase increases from a D or F to any other grade. For example, from a D to an A.
+
+An area of your codebase decreases to a D or F from any other grade. For example, from a B to an F.
+
+New code is added to your codebase and its initial grade is a D or F.
+
+Test coverage improves or declines.
+
+By default, emails for this event are disabled. You can enable them by editing your personal email preferences.
+By default, you’ll receive one weekly summary per repository that has received at least one commit in the past week. Weekly summaries are sent only via email, not via integration notifications.
+TABLE OF CONTENTS
+Progress Events
+Email and Integration Events
+One of Code Climate’s primary focuses is to provide insight and visibility into how your codebase is changing over time. After we analyze a new commit, we’ll share our findings with you in various ways.
+
+Progress Events
+Your repository’s Progress page is the main hub for you to review Code Climate’s analysis data.
+
+When any of the following events occur during an analysis of your default branch, we’ll update your Progress report:
+
+An area of your codebase has its grade increase or decrease from one letter grade to another.
+
+New code is added to your codebase. The Progress report will show you its initial grade.
+
+Test coverage improves or declines.
+
+We’ll also post a weekly summary to your Progress report (on Monday), which shows information like how many files were changed, inserted, and deleted in the past seven days.
+
+The very first analysis that Code Climate runs on your repository (directly after the repository is added) will never result in any Progress report updates. Only the second and subsequent analyses can produce Progress report updates.
+Email and Integration Events
+We understand that you may not have time to check the Code Climate website every day, so we’ll also notify you via email and chatroom integrations. To reduce noise, we’re more conservative about when we send notifications.
+
+Below are all possible notifications that can result from an analysis of your default branch:
+
+An area of your codebase increases from a D or F to any other grade. For example, from a D to an A.
+
+An area of your codebase decreases to a D or F from any other grade. For example, from a B to an F.
+
+New code is added to your codebase and its initial grade is a D or F.
+
+Test coverage improves or declines.
+
+By default, emails for this event are disabled. You can enable them by editing your personal email preferences.
+By default, you’ll receive one weekly summary per repository that has received at least one commit in the past week. Weekly summaries are sent only via email, not via integration notifications.
+TABLE OF CONTENTS
+Progress Events
+Email and Integration Events
+One of Code Climate’s primary focuses is to provide insight and visibility into how your codebase is changing over time. After we analyze a new commit, we’ll share our findings with you in various ways.
+
+Progress Events
+Your repository’s Progress page is the main hub for you to review Code Climate’s analysis data.
+
+When any of the following events occur during an analysis of your default branch, we’ll update your Progress report:
+
+An area of your codebase has its grade increase or decrease from one letter grade to another.
+
+New code is added to your codebase. The Progress report will show you its initial grade.
+
+Test coverage improves or declines.
+
+We’ll also post a weekly summary to your Progress report (on Monday), which shows information like how many files were changed, inserted, and deleted in the past seven days.
+
+The very first analysis that Code Climate runs on your repository (directly after the repository is added) will never result in any Progress report updates. Only the second and subsequent analyses can produce Progress report updates.
+Email and Integration Events
+We understand that you may not have time to check the Code Climate website every day, so we’ll also notify you via email and chatroom integrations. To reduce noise, we’re more conservative about when we send notifications.
+
+Below are all possible notifications that can result from an analysis of your default branch:
+
+An area of your codebase increases from a D or F to any other grade. For example, from a D to an A.
+
+An area of your codebase decreases to a D or F from any other grade. For example, from a B to an F.
+
+New code is added to your codebase and its initial grade is a D or F.
+
+Test coverage improves or declines.
+
+By default, emails for this event are disabled. You can enable them by editing your personal email preferences.
+By default, you’ll receive one weekly summary per repository that has received at least one commit in the past week. Weekly summaries are sent only via email, not via integration notifications.
+TABLE OF CONTENTS
+Progress Events
+Email and Integration Events
+One of Code Climate’s primary focuses is to provide insight and visibility into how your codebase is changing over time. After we analyze a new commit, we’ll share our findings with you in various ways.
+
+Progress Events
+Your repository’s Progress page is the main hub for you to review Code Climate’s analysis data.
+
+When any of the following events occur during an analysis of your default branch, we’ll update your Progress report:
+
+An area of your codebase has its grade increase or decrease from one letter grade to another.
+
+New code is added to your codebase. The Progress report will show you its initial grade.
+
+Test coverage improves or declines.
+
+We’ll also post a weekly summary to your Progress report (on Monday), which shows information like how many files were changed, inserted, and deleted in the past seven days.
+
+The very first analysis that Code Climate runs on your repository (directly after the repository is added) will never result in any Progress report updates. Only the second and subsequent analyses can produce Progress report updates.
+Email and Integration Events
+We understand that you may not have time to check the Code Climate website every day, so we’ll also notify you via email and chatroom integrations. To reduce noise, we’re more conservative about when we send notifications.
+
+Below are all possible notifications that can result from an analysis of your default branch:
+
+An area of your codebase increases from a D or F to any other grade. For example, from a D to an A.
+
+An area of your codebase decreases to a D or F from any other grade. For example, from a B to an F.
+
+New code is added to your codebase and its initial grade is a D or F.
+
+Test coverage improves or declines.
+
+By default, emails for this event are disabled. You can enable them by editing your personal email preferences.
+By default, you’ll receive one weekly summary per repository that has received at least one commit in the past week. Weekly summaries are sent only via email, not via integration notifications.
+TABLE OF CONTENTS
+Progress Events
+Email and Integration Events
+One of Code Climate’s primary focuses is to provide insight and visibility into how your codebase is changing over time. After we analyze a new commit, we’ll share our findings with you in various ways.
+
+Progress Events
+Your repository’s Progress page is the main hub for you to review Code Climate’s analysis data.
+
+When any of the following events occur during an analysis of your default branch, we’ll update your Progress report:
+
+An area of your codebase has its grade increase or decrease from one letter grade to another.
+
+New code is added to your codebase. The Progress report will show you its initial grade.
+
+Test coverage improves or declines.
+
+We’ll also post a weekly summary to your Progress report (on Monday), which shows information like how many files were changed, inserted, and deleted in the past seven days.
+
+The very first analysis that Code Climate runs on your repository (directly after the repository is added) will never result in any Progress report updates. Only the second and subsequent analyses can produce Progress report updates.
+Email and Integration Events
+We understand that you may not have time to check the Code Climate website every day, so we’ll also notify you via email and chatroom integrations. To reduce noise, we’re more conservative about when we send notifications.
+
+Below are all possible notifications that can result from an analysis of your default branch:
+
+An area of your codebase increases from a D or F to any other grade. For example, from a D to an A.
+
+An area of your codebase decreases to a D or F from any other grade. For example, from a B to an F.
+
+New code is added to your codebase and its initial grade is a D or F.
+
+Test coverage improves or declines.
+
+By default, emails for this event are disabled. You can enable them by editing your personal email preferences.
+By default, you’ll receive one weekly summary per repository that has received at least one commit in the past week. Weekly summaries are sent only via email, not via integration notifications.
+TABLE OF CONTENTS
+Progress Events
+Email and Integration Events
+One of Code Climate’s primary focuses is to provide insight and visibility into how your codebase is changing over time. After we analyze a new commit, we’ll share our findings with you in various ways.
+
+Progress Events
+Your repository’s Progress page is the main hub for you to review Code Climate’s analysis data.
+
+When any of the following events occur during an analysis of your default branch, we’ll update your Progress report:
+
+An area of your codebase has its grade increase or decrease from one letter grade to another.
+
+New code is added to your codebase. The Progress report will show you its initial grade.
+
+Test coverage improves or declines.
+
+We’ll also post a weekly summary to your Progress report (on Monday), which shows information like how many files were changed, inserted, and deleted in the past seven days.
+
+The very first analysis that Code Climate runs on your repository (directly after the repository is added) will never result in any Progress report updates. Only the second and subsequent analyses can produce Progress report updates.
+Email and Integration Events
+We understand that you may not have time to check the Code Climate website every day, so we’ll also notify you via email and chatroom integrations. To reduce noise, we’re more conservative about when we send notifications.
+
+Below are all possible notifications that can result from an analysis of your default branch:
+
+An area of your codebase increases from a D or F to any other grade. For example, from a D to an A.
+
+An area of your codebase decreases to a D or F from any other grade. For example, from a B to an F.
+
+New code is added to your codebase and its initial grade is a D or F.
+
+Test coverage improves or declines.
+
+By default, emails for this event are disabled. You can enable them by editing your personal email preferences.
+By default, you’ll receive one weekly summary per repository that has received at least one commit in the past week. Weekly summaries are sent only via email, not via integration notifications.
+TABLE OF CONTENTS
+Progress Events
+Email and Integration Events
